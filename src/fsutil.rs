b5ccff6d37d3f1e2513d83257a4a64206734b877
Aaron Power <theaaronepower@gmail.com>
2015-09-21 20:48:07 +0100

    VERSION 1.1
    - Added possible values for `--sort` `[files, total, blanks, code, comments]`
    
    - Added support for the following languages
      - FORTRAN Legacy
      - FORTRAN Modern
      - Julia

--- src/fsutil.rs
+++ src/fsutil.rs
@@ -5,87 +5,87 @@ use std::fs::metadata;
 use self::glob::glob;
 
 pub fn contains_comments(file: &str, comment: &str) -> bool {
-	let vector = file.splitn(3, "\"").filter_map( |element| {
-		if !(element == "") {
-			Some(element)
-		} else {
-			None
-		}
-		
-	}).collect::<Vec<&str>>();
+    let vector = file.splitn(3, "\"").filter_map( |element| {
+        if !(element == "") {
+            Some(element)
+        } else {
+            None
+        }
+        
+    }).collect::<Vec<&str>>();
 
-	let length = vector.len();
+    let length = vector.len();
 
-	if length == 0 || length == 1 {
-		return false
-	}
+    if length == 0 || length == 1 {
+        return false
+    }
 
-	if length == 2 {
-		for element in &vector {
-			if element.contains(comment) {
-				return true
-			}
-		}
-		return false
-	}
+    if length == 2 {
+        for element in &vector {
+            if element.contains(comment) {
+                return true
+            }
+        }
+        return false
+    }
 
-	if vector[0].contains(comment) {
+    if vector[0].contains(comment) {
 
-		return true
-	}
+        return true
+    }
 
-	if vector[2].contains("\"") {
+    if vector[2].contains("\"") {
 
-		return contains_comments(vector[2], comment)
-	} else if vector[2].contains(comment) {
+        return contains_comments(vector[2], comment)
+    } else if vector[2].contains(comment) {
 
-		return true
-	}
-	false
+        return true
+    }
+    false
 }
 
 pub fn get_all_files(path: String, ignored_directories: &Vec<String>) -> Vec<String> {
-	let mut files: Vec<String> = Vec::new();
+    let mut files: Vec<String> = Vec::new();
 
-	if let Ok(result) = metadata(&path) {
-		if result.is_dir() {
-			let dir = match fs::read_dir(&path) {
+    if let Ok(result) = metadata(&path) {
+        if result.is_dir() {
+            let dir = match fs::read_dir(&path) {
                 Ok(value) => value,
                 Err(err) => panic!("ERROR: {:?}", err),
             };
-			'file: for entry in dir {
-				let entry = unwrap_rs_cont!(entry);
-				let file_path = entry.path();
-				let file_str = unwrap_opt_cont!(file_path.to_str()); 
-				let file_string = file_str.to_owned();
-				let path_metadata = unwrap_rs_cont!(metadata(file_str));
+            'file: for entry in dir {
+                let entry = unwrap_rs_cont!(entry);
+                let file_path = entry.path();
+                let file_str = unwrap_opt_cont!(file_path.to_str()); 
+                let file_string = file_str.to_owned();
+                let path_metadata = unwrap_rs_cont!(metadata(file_str));
 
-				if path_metadata.is_dir() {
-					for ignored_directory in ignored_directories {
-						if file_str.contains(ignored_directory) {
-							continue 'file;
-						}
-					}
-					for file in get_all_files(file_string, &ignored_directories) {
-						files.push(file);
-					}
-				} else if path_metadata.is_file() {
-					files.push(file_string);
-				}
-			}
-		} else {
-			files.push(path);
-		}
-	} else {
+                if path_metadata.is_dir() {
+                    for ignored_directory in ignored_directories {
+                        if file_str.contains(ignored_directory) {
+                            continue 'file;
+                        }
+                    }
+                    for file in get_all_files(file_string, &ignored_directories) {
+                        files.push(file);
+                    }
+                } else if path_metadata.is_file() {
+                    files.push(file_string);
+                }
+            }
+        } else {
+            files.push(path);
+        }
+    } else {
         let iter = match glob(&path) {
             Ok(value) => value,
             Err(err) => panic!("{:?}", err)
         }; 
-		for path_buf in iter {
-			let file_path = unwrap_opt_cont!(unwrap_rs_cont!(path_buf).as_path().to_str()).to_owned();
-			files.push(file_path);
-		}
-	}
+        for path_buf in iter {
+            let file_path = unwrap_opt_cont!(unwrap_rs_cont!(path_buf).as_path().to_str()).to_owned();
+            files.push(file_path);
+        }
+    }
 
-	files
+    files
 }
--- src/main.rs
+++ src/main.rs
@@ -20,54 +20,54 @@ static ROW: &'static str = "----------------------------------------------------
 
 fn main() {
     let yaml = load_yaml!("../cli.yml");
-	let matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
 
-	let mut languages: BTreeMap<&str, Language> = BTreeMap::new();
-	languages.insert("as"     , Language::new_c("ActionScript"));
-	languages.insert("c"      , Language::new_c("C"));
-	languages.insert("cs"     , Language::new_c("C#"));
-	languages.insert("clj"    , Language::new_single("Clojure", ";,#,#_"));
-	languages.insert("coffee" , Language::new("CoffeeScript", "#", "###", "###"));
-	languages.insert("cfm"    , Language::new("ColdFusion", "<!---", "<!---", "--->"));
-	languages.insert("cfc"    , Language::new_c("ColdFusion CFScript"));
-	languages.insert("cpp"    , Language::new_c("C++"));
-	languages.insert("css"    , Language::new_c("CSS"));
-	languages.insert("d"      , Language::new_c("D"));
-	languages.insert("dart"   , Language::new_c("Dart"));
-	languages.insert("f90"    , Language::new_single("FORTRAN Modern", "!"));
-	languages.insert("f"      , Language::new_single("FORTRAN Legacy", "c,C,!,*"));
-	languages.insert("go"     , Language::new_c("Go"));
-	languages.insert("h"      , Language::new_c("C Header"));
-	languages.insert("hs"     , Language::new_single("Haskell", "--"));
-	languages.insert("hpp"    , Language::new_c("C++ Header"));
-	languages.insert("html"   , Language::new_html("HTML"));
-	languages.insert("java"   , Language::new_c("Java"));
-	languages.insert("js"     , Language::new_c("JavaScript"));
+    let mut languages: BTreeMap<&str, Language> = BTreeMap::new();
+    languages.insert("as"     , Language::new_c("ActionScript"));
+    languages.insert("c"      , Language::new_c("C"));
+    languages.insert("cs"     , Language::new_c("C#"));
+    languages.insert("clj"    , Language::new_single("Clojure", ";,#,#_"));
+    languages.insert("coffee" , Language::new("CoffeeScript", "#", "###", "###"));
+    languages.insert("cfm"    , Language::new("ColdFusion", "<!---", "<!---", "--->"));
+    languages.insert("cfc"    , Language::new_c("ColdFusion CFScript"));
+    languages.insert("cpp"    , Language::new_c("C++"));
+    languages.insert("css"    , Language::new_c("CSS"));
+    languages.insert("d"      , Language::new_c("D"));
+    languages.insert("dart"   , Language::new_c("Dart"));
+    languages.insert("f90"    , Language::new_single("FORTRAN Modern", "!"));
+    languages.insert("f"      , Language::new_single("FORTRAN Legacy", "c,C,!,*"));
+    languages.insert("go"     , Language::new_c("Go"));
+    languages.insert("h"      , Language::new_c("C Header"));
+    languages.insert("hs"     , Language::new_single("Haskell", "--"));
+    languages.insert("hpp"    , Language::new_c("C++ Header"));
+    languages.insert("html"   , Language::new_html("HTML"));
+    languages.insert("java"   , Language::new_c("Java"));
+    languages.insert("js"     , Language::new_c("JavaScript"));
     languages.insert("jl"     , Language::new("Julia", "#", "#=", "=#"));
-	languages.insert("json"   , Language::new_blank("JSON"));
-	languages.insert("jsx"    , Language::new_c("JSX"));
-	languages.insert("el"     , Language::new("LISP", ";", "#|", "|#"));
-	languages.insert("m"      , Language::new_c("Objective-C"));
-	languages.insert("mm"     , Language::new_c("Objective-C++"));
-	languages.insert("php"    , Language::new("PHP", "#,//","/*","*/"));
-	languages.insert("pas"    , Language::new("Pascal", "//,(*","{","}"));
-	languages.insert("pl"     , Language::new("Perl", "#","=","=cut"));
-	languages.insert("py"     , Language::new("Python", "#","'''","'''"));
-	languages.insert("rs"     , Language::new("Rust", "//,///,//!", "/*", "*/"));
-	languages.insert("r"      , Language::new("R", "#","",""));
-	languages.insert("rb"     , Language::new("Ruby", "#","=begin","=end"));
-	languages.insert("rhtml"  , Language::new_html("Ruby HTML"));
-	languages.insert("sass"   , Language::new_c("Sass"));
-	languages.insert("sh"     , Language::new_single("BASH", "#"));
-	languages.insert("sql"    , Language::new("SQL", "--", "/*", "*/"));
-	languages.insert("swift"  , Language::new_c("Swift"));
-	languages.insert("ts"     , Language::new_c("TypeScript"));
-	languages.insert("xml"    , Language::new_html("XML"));
-	languages.insert("yml"    , Language::new_single("YAML", "#"));
+    languages.insert("json"   , Language::new_blank("JSON"));
+    languages.insert("jsx"    , Language::new_c("JSX"));
+    languages.insert("el"     , Language::new("LISP", ";", "#|", "|#"));
+    languages.insert("m"      , Language::new_c("Objective-C"));
+    languages.insert("mm"     , Language::new_c("Objective-C++"));
+    languages.insert("php"    , Language::new("PHP", "#,//","/*","*/"));
+    languages.insert("pas"    , Language::new("Pascal", "//,(*","{","}"));
+    languages.insert("pl"     , Language::new("Perl", "#","=","=cut"));
+    languages.insert("py"     , Language::new("Python", "#","'''","'''"));
+    languages.insert("rs"     , Language::new("Rust", "//,///,//!", "/*", "*/"));
+    languages.insert("r"      , Language::new("R", "#","",""));
+    languages.insert("rb"     , Language::new("Ruby", "#","=begin","=end"));
+    languages.insert("rhtml"  , Language::new_html("Ruby HTML"));
+    languages.insert("sass"   , Language::new_c("Sass"));
+    languages.insert("sh"     , Language::new_single("BASH", "#"));
+    languages.insert("sql"    , Language::new("SQL", "--", "/*", "*/"));
+    languages.insert("swift"  , Language::new_c("Swift"));
+    languages.insert("ts"     , Language::new_c("TypeScript"));
+    languages.insert("xml"    , Language::new_html("XML"));
+    languages.insert("yml"    , Language::new_single("YAML", "#"));
 
     if matches.is_present("languages") {
         for (ext, language) in languages {
-        println!("{:<25} ({})", language.name, ext);
+            println!("{:<25} ({})", language.name, ext);
         }
         return;
     }
@@ -77,11 +77,11 @@ fn main() {
 
     let mut ignored_directories: Vec<String> = Vec::new();
 
-	if let Some(user_ignored) = matches.values_of("exclude") {
-		for ignored in user_ignored {
+    if let Some(user_ignored) = matches.values_of("exclude") {
+        for ignored in user_ignored {
             ignored_directories.push(ignored.to_owned());
         }
-	}
+    }
 
     let mut sort = String::new();
     if let Some(sort_by) = matches.value_of("sort") {
@@ -92,40 +92,37 @@ fn main() {
     }
     let sort_empty = sort.is_empty();
 
-	println!("{}", ROW);
-	println!(" {:<15} {:>15} {:>15} {:>15} {:>15} {:>15}",
-		"Language", "Files", "Total", "Blanks", "Comments", "Code");
-	println!("{}", ROW);
-		for path in paths {
-		let files = get_all_files(path.to_owned(), &ignored_directories);
+    println!("{}", ROW);
+    println!(" {:<15} {:>15} {:>15} {:>15} {:>15} {:>15}",
+             "Language", "Files", "Total", "Blanks", "Comments", "Code");
+    println!("{}", ROW);
+    for path in paths {
+        let files = get_all_files(path.to_owned(), &ignored_directories);
 
-		for file in files {
-			let extension = unwrap_opt_cont!(unwrap_opt_cont!(Path::new(&file).extension()).to_str());
+        for file in files {
+            let extension = unwrap_opt_cont!(unwrap_opt_cont!(Path::new(&file).extension()).to_str());
 
-			let lowercase: &str = &extension.to_lowercase();
+            let lowercase: &str = &extension.to_lowercase();
 
-			let mut language = unwrap_opt_cont!(languages.get_mut(lowercase));
-			language.files.push(file.to_owned());
-		}
-	}
+            let mut language = unwrap_opt_cont!(languages.get_mut(lowercase));
+            language.files.push(file.to_owned());
+        }
+    }
 
-	let mut total = Language::new_blank("Total");
+    let mut total = Language::new_blank("Total");
 
-	for (_, language) in &mut languages {
+    for (_, language) in &mut languages {
 
-		for file in language.files.iter() {
+        for file in language.files.iter() {
 
-			let mut file_ref = unwrap_rs_cont!(File::open(&file));
-			let mut contents = String::new();
+            let mut contents = String::new();
             let is_fortran = language.name.contains("FORTRAN");
+            let _ = unwrap_rs_cont!(unwrap_rs_cont!(File::open(&file)).read_to_string(&mut contents));
 
-			let _ = unwrap_rs_cont!(file_ref.read_to_string(&mut contents));
-
-			let mut is_in_comments = false;
-
-			'line: for line in contents.lines() {
-				let line = if is_fortran {line} else {line.trim()};
-				language.lines += 1;
+            let mut is_in_comments = false;
+            'line: for line in contents.lines() {
+                let line = if is_fortran {line} else {line.trim()};
+                language.lines += 1;
 
                 if line.trim().is_empty() {
                     language.blanks += 1;
@@ -140,35 +137,37 @@ fn main() {
                     }
                 }
 
-				if is_in_comments {
-					if line.contains(language.multi_line_end) {
-						is_in_comments = false;
-					}
-					language.comments += 1;
-					continue;
-				}
-				let single_comments = language.line_comment.split(",");
-				for single in single_comments {
-					if line.starts_with(single) {
-						language.comments += 1;
-                        continue 'line;
-					}
+                if is_in_comments {
+                    if line.contains(language.multi_line_end) {
+                        is_in_comments = false;
+                    }
+                    language.comments += 1;
+                    continue;
+                }
+                if !language.line_comment.is_empty() {
+                    let single_comments = language.line_comment.split(",");
+                    for single in single_comments {
+                        if line.starts_with(single) {
+                            language.comments += 1;
+                            continue 'line;
+                        }
+                    }
                 }
                 language.code += 1;
-			}
-		}
+            }
+        }
 
 
-		if !language.is_empty() && sort_empty {
-			println!("{}", language);
-		}
+        if !language.is_empty() && sort_empty {
+            println!("{}", language);
+        }
 
-		total.total    += language.files.len();
-		total.lines    += language.lines;
-		total.comments += language.comments;
-		total.blanks   += language.blanks;
-		total.code     += language.code;
-	}
+        total.total    += language.files.len();
+        total.lines    += language.lines;
+        total.comments += language.comments;
+        total.blanks   += language.blanks;
+        total.code     += language.code;
+    }
 
     if !sort_empty {
         let mut unsorted_vec:Vec<(&&str, &Language)> = languages.iter().collect();
@@ -198,7 +197,7 @@ fn main() {
         }
     }
 
-	println!("{}", ROW);
-	println!("{}", total);
-	println!("{}", ROW);
+    println!("{}", ROW);
+    println!("{}", total);
+    println!("{}", ROW);
 }
