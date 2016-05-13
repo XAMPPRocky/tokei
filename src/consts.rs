8c6b6f28b2bf15186e13ab7c197c91b9572498d5
Aaronepower <theaaronepower@gmail.com>
2016-05-13 22:16:20 +0100

    forgot file

---
+++ src/consts.rs
@@ -0,0 +1,6 @@
+pub const ROW: &'static str = "-------------------------------------------------------------------------------";
+pub const BLANKS: &'static str = "blanks";
+pub const COMMENTS: &'static str = "comments";
+pub const CODE: &'static str = "code";
+pub const FILES: &'static str = "files";
+pub const TOTAL: &'static str = "total";
