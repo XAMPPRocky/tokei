87a7b44af5f22b48a1927fadf160321718cd934e
Aaronepower <theaaronepower@gmail.com>
2016-08-01 14:01:12 +0100

    Increased stack size for builds

--- src/lib/build.rs
+++ src/lib/build.rs
@@ -9,13 +9,22 @@ fn main() {
 fn expand() {
     use std::env;
     use std::path::Path;
+    use std::thread;
 
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let builder = thread::Builder::new()
+        .name(String::from("Build Thread"))
+        .stack_size(8388608);
 
-    let src = Path::new("src/lib/lib.rs.in");
-    let dst = Path::new(&out_dir).join("lib.rs.in");
+    let handle = builder.spawn(|| {
+        let out_dir = env::var_os("OUT_DIR").unwrap();
 
-    serde_codegen::expand(&src, &dst).unwrap();
+        let src = Path::new("src/lib/lib.rs.in");
+        let dst = Path::new(&out_dir).join("lib.rs.in");
+
+        serde_codegen::expand(&src, &dst).unwrap();
+    });
+
+    let _ = handle.unwrap().join();
 }
 
 #[cfg(not(feature = "io"))]
