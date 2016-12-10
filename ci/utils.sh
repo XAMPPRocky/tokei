ed7ba866ccdb7d92e0756cefc2cab9a35200eca6
Aaron Power <theaaronepower@gmail.com>
2016-12-10 19:40:53 +0000

    Fixed musl builds

--- ci/utils.sh
+++ ci/utils.sh
@@ -37,7 +37,7 @@ dobin() {
     local f prefix=$(gcc_prefix)
     for f in "$@"; do
         install -m0755 $f $dtd/debian/usr/bin/
-        ${prefix}strip -s $dtd/debian/usr/bin/$(basename $f)
+        strip -s $dtd/debian/usr/bin/$(basename $f)
     done
 }
 
