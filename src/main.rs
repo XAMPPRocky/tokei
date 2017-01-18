XAMPPRocky/tokei

commit b7a892f4753de6816de88e4df231e3ec7502269d

v5.0.6: Updated dependencies, fixed exclude bug, and now defaults to running the current directory over failing.

cli.yml
@@ -29,6 +29,7 @@ args:
         index: 1
         multiple: true
         required: true
+        default_value: "."
     - languages:
         conflicts_with:
             - input

Cargo.toml
@@ -8,7 +8,7 @@ license = "MIT/Apache-2.0"
 name = "tokei"
 readme = "README.md"
 repository = "https://github.com/Aaronepower/tokei.git"
-version = "5.0.5"
+version = "5.0.6"
 
 [profile.release]
 panic="abort"
@@ -34,7 +34,7 @@ lazy_static = "0.2"
 log = "0.3"
 maplit = "0.1"
 rayon = "0.6"
-regex = "0.1"
+regex = "0.2"
 
 [dependencies.clap]
 features = ["yaml"]

src/language/language.rs
@@ -18,7 +18,7 @@ include!(concat!(env!("OUT_DIR"), "/language_serde_types.rs"));
 fn generate_regex(multi_line: &[(&'static str, &'static str)]) -> Cow<'static, Regex> {
     let mut raw_regex = String::new();
     for &(start, _) in multi_line {
-        raw_regex.push_str(&regex::quote(start));
+        raw_regex.push_str(&regex::escape(start));
         raw_regex.push_str("|");
     }
     let _ = raw_regex.pop();
@@ -101,7 +101,7 @@ impl Language {
     /// ```
     pub fn new_func() -> Self {
         lazy_static! {
-            static ref FUNC_REGEX: Regex = Regex::new(&regex::quote(r"\(\*")).unwrap();
+            static ref FUNC_REGEX: Regex = Regex::new(r"\(\*").unwrap();
         }
         Language {
             multi_line: vec![("(*", "*)")],
@@ -162,7 +162,7 @@ impl Language {
     /// ```
     pub fn new_haskell() -> Self {
         lazy_static! {
-            static ref HASKELL_REGEX: Regex = Regex::new(&regex::quote(r"\{-")).unwrap();
+            static ref HASKELL_REGEX: Regex = Regex::new(r"\{-").unwrap();
         }
 
         Language {

src/utils/fs.rs
@@ -30,16 +30,13 @@ pub fn get_all_files(paths: Vec<&str>,
     }
 
     if !ignored_directories.is_empty() {
-        let mut ignored_directories = ignored_directories.iter();
-        let mut overrides = OverrideBuilder::new(
-            &format!("!{}", ignored_directories.next().unwrap())
-        );
+        let mut overrides = OverrideBuilder::new(".");
 
         for ignored in ignored_directories {
             rs_error!(overrides.add(&format!("!{}", ignored)));
         }
 
-        walker.overrides(overrides.build().unwrap());
+        walker.overrides(overrides.build().expect("Excludes were in invalid"));
     }
 
     walker.build_parallel().run(move|| {

tests/accuracy.rs
@@ -18,8 +18,8 @@ lazy_static! {
 
 macro_rules! get_digit {
     ($regex:expr, $text:expr) => {{
-        let (begin, end) = $regex.find(&$text).expect("Couldn't find category");
-        $text[begin..end].split_whitespace()
+        let matched = $regex.find(&$text).expect("Couldn't find category");
+        matched.as_str().split_whitespace()
             .next()
             .unwrap()
             .parse::<usize>()

commit 6de6c0b464c16ffb7a00e72823767c686452adc3

fixed `--languages` option always conflicting with input
As defined in `cli.yml`, `--languages` conflicts with `<input>`,
but if `<input>` has default value, `--languages` will always conflict,
even if no inputs were specified.

Computation of the default value for `<input>` is now moved to `src/main.rs`,
because it cannot be specified in `cli.yml` along with `--languages`.

I suspect this bug was introduced in commit b7a892f4753d, when default
value for `<input>` was added.

cli.yml
@@ -29,7 +29,6 @@ args:
         index: 1
         multiple: true
         required: true
-        default_value: "."
     - languages:
         conflicts_with:
             - input

src/main.rs
@@ -66,7 +66,10 @@ fn main() {
         return;
     }
 
-    let paths: Vec<&str> = matches.values_of("input").unwrap().collect();
+    let paths: Vec<&str> = match matches.values_of("input") {
+        Some(vs) => vs.collect(),
+        None => vec!["."],
+    };
 
     if let Some(input) = input_option {
         add_input(input, &mut languages);