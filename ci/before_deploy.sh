3291ed9fef9f3b9e69acfa8101b7328d6869ba99
alexey zabelin <zabelin.alex@gmail.com>
2017-05-04 22:36:01 -0400

    Move CI to trust
    These changes configure the trust template to use the correct
    credentials and project name. The rest was left as is.
    
    There seems to be an issue with rustup at the moment
    https://github.com/rust-lang-nursery/rustup.rs/issues/1092. As suggested in
    https://github.com/rust-lang-nursery/rustup.rs/issues/1092#issuecomment-299454982
    I've enabled the `allow_failures` feature for nightly builds.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,65 +1,33 @@
-# `before_deploy` phase: here we package the build artifacts
+# This script takes care of building your crate and packaging it for release
 
 set -ex
 
-. $(dirname $0)/utils.sh
-
-# Generate artifacts for release
-mk_artifacts() {
-    cargo build --target $TARGET --release --features all
-}
-
-mk_tarball() {
-    # create a "staging" directory
-    local td=$(mktempd)
-    local out_dir=$(pwd)
-
-    cp target/$TARGET/release/tokei $td
-
-    pushd $td
-
-    # release tarball will look like 'rust-everywhere-v1.2.3-x86_64-unknown-linux-gnu.tar.gz'
-    tar czf $out_dir/${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}.tar.gz *
-
-    popd
-    rm -r $td
-}
-
-# Package your artifacts in a .deb file
-# NOTE right now you can only package binaries using the `dobin` command. Simply call
-# `dobin [file..]` to include one or more binaries in your .deb package. I'll add more commands to
-# install other things like manpages (`doman`) as the needs arise.
-# XXX This .deb packaging is minimal -- just to make your app installable via `dpkg` -- and doesn't
-# fully conform to Debian packaging guideliens (`lintian` raises a few warnings/errors)
-mk_deb() {
-    dobin target/$TARGET/release/tokei
-}
-
 main() {
-    mk_artifacts
-    mk_tarball
+    local src=$(pwd) \
+          stage=
 
-    if [ $TRAVIS_OS_NAME = linux ]; then
-        if [ ! -z $MAKE_DEB ]; then
-            dtd=$(mktempd)
-            mkdir -p $dtd/debian/usr/bin
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
 
-            mk_deb
+    test -f Cargo.lock || cargo generate-lockfile
 
-            mkdir -p $dtd/debian/DEBIAN
-            cat >$dtd/debian/DEBIAN/control <<EOF
-Package: $PROJECT_NAME
-Version: ${TRAVIS_TAG#v}
-Architecture: $(architecture $TARGET)
-Maintainer: $DEB_MAINTAINER
-Description: $DEB_DESCRIPTION
-EOF
+    # TODO Update this to build the artifacts that matter to you
+    cross rustc --bin tokei --target $TARGET --release -- -C lto
 
-            fakeroot dpkg-deb --build $dtd/debian
-            mv $dtd/debian.deb $PROJECT_NAME-$TRAVIS_TAG-$TARGET.deb
-            rm -r $dtd
-        fi
-    fi
+    # TODO Update this to package the right artifacts
+    cp target/$TARGET/release/tokei $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
 }
 
 main
--- ci/script.sh
+++ ci/script.sh
@@ -1,42 +1,24 @@
-# `script` phase: you usually build, test and generate docs in this phase
+# This script takes care of testing your crate
 
 set -ex
 
-. $(dirname $0)/utils.sh
+# TODO This is the "test phase", tweak it as you see fit
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
 
-# TODO modify this function as you see fit
-# PROTIP Always pass `--target $TARGET` to cargo commands, this makes cargo output build artifacts
-# to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
-# `before_deploy`/packaging phase
-run_test_suite() {
-    case $TARGET in
-        # configure emulation for transparent execution of foreign binaries
-        aarch64-unknown-linux-gnu)
-            export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
-            ;;
-        arm*-unknown-linux-gnueabihf)
-            export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
-            ;;
-        *)
-            ;;
-    esac
-
-    if [ ! -z "$QEMU_LD_PREFIX" ]; then
-        # Run tests on a single thread when using QEMU user emulation
-        export RUST_TEST_THREADS=1
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
     fi
 
-    cargo build --target $TARGET --verbose
-    cargo test --target $TARGET
-    cargo build --target $TARGET --verbose --release --features=all
-    cargo test --target $TARGET --release --features=all
+    cross test --target $TARGET
+    cross test --target $TARGET --release
 
-    # sanity check the file type
-    file target/$TARGET/debug/tokei
+    cross run --target $TARGET
+    cross run --target $TARGET --release
 }
 
-main() {
-    run_test_suite
-}
-
-main
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
