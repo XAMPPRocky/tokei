2bcc3b5508d49360eafea83a1b65e663b1ad2adf
si1kdd <silkdd.davsullivan@zoho.com>
2017-07-31 02:55:52 +0800

    Add tests for python2 and html files

---
+++ tests/data/python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Comments
+# Comments
+# Comments
+# Comments
+
+
+def foo(arg1):
+    """Multiple Line Comments.
+        Comments
+        Comments
+        Comments
+    """
+    pass
+
+
+foo()
