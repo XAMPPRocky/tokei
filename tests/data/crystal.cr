30bff141d13dc0693a9efdf0f73a2994ea269196
Márton Szabó <notramo@vipmail.hu>
2017-08-18 15:34:30 +0200

    Added test file for Crystal language

---
+++ tests/data/crystal.cr
@@ -0,0 +1,20 @@
+# 20 lines 14 code 2 comments 4 blanks
+x = 3
+if x < 2
+  p = "Smaller"
+else
+  p = "Bigger"
+end
+
+multiline_string = "first line
+second line"
+
+heredoc = <<-SOME
+hello
+SOME
+
+# testing.
+while x > 2 && x < 10
+  x += 1
+end
+
