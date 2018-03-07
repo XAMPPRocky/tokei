86bb1ad144833e70d93d285c1f968e374a23e5bc
Marc Tiehuis <marctiehuis@gmail.com>
2018-03-07 17:39:22 +1300

    Add support for zig

---
+++ tests/data/zig.zig
@@ -0,0 +1,11 @@
+// 11 lines 5 code 3 comments 3 blanks
+
+/// Documentation comment
+pub fn main() void {
+    const a = 5; // not-counted
+
+    // Leading-comment
+    const b = c"line comment embedded //";
+    const c = \\line comment embedded // //
+}
+
