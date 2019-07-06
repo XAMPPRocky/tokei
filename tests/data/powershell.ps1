b36284f9e13e854526a9f247fad76ca16a05f714
Jason Shirk <jason@truewheels.net>
2019-07-06 01:36:55 -0700

    Add PowerShell support (#346)
    Resolves #339

---
+++ tests/data/powershell.ps1
@@ -0,0 +1,17 @@
+# 17 lines 9 code 4 comments 4 blanks
+
+<#
+Test
+#>
+
+'a' + "b"
+
+Write-Host @"
+    Name: $name
+    Address: $address
+"@
+
+$template = @'
+    Name: {0}
+    Address: {0}
+'@
