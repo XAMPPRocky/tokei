4b159a8c7d49bf0df4b19d3f3406e3063b8be8f6
Samantha <rosasynstylae@gmail.com>
2020-05-07 12:01:39 -0600

    Separate TSX and Typescript Support
    * Follows the JSX and Javascript separation
    * Separate TSX and Typescript for clearer stats
    * Add TSX test

---
+++ tests/data/tsx.tsx
@@ -0,0 +1,9 @@
+// 9 lines, 5 code, 3 comments, 1 blanks
+
+/** string two numbers together */
+const stringNums = (x: number, y: number) => {
+  // the line below makes a string
+  const firstNum = x + "";
+  const secondNum = firstNum + y;
+  return secondNum;
+};
