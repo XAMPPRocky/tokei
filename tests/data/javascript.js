7ea0feb645aea00632cdad92373ce08dacf380b2
NickHackman <snickhackman@gmail.com>
2020-05-09 22:49:47 -0400

    Javascript test

---
+++ tests/data/javascript.js
@@ -0,0 +1,33 @@
+// 33 lines, 14 code, 11 comments, 8 blanks
+
+/*
+ * /* Nested comment
+ * // single line comment
+ * */
+
+/*
+
+function add(a, b) {
+  return a + b;
+}
+*/
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.width * this.height;
+  }
+}
+
+let rect = new Rectangle(20, 20);
+console.log(rect.area); // 400
+
+// Comment
