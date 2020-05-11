d3dd4e55bf749e2c488c9b83a7ea3f88e3687f83
NickHackman <snickhackman@gmail.com>
2020-05-10 23:29:00 -0400

    Added Vue Test

---
+++ tests/data/vue.vue
@@ -0,0 +1,36 @@
+<!-- 36 lines, 24 code, 9 comments, 3 blanks -->
+<template>
+  <div id="app">
+    <button v-on:click="clicked">
+      <!-- Button that increments count -->
+      Clicked {{ count }} {{ count == 1 ? "time" : "times" }}
+    </button>
+  </div>
+</template>
+
+<script>
+/*
+ Javascript Section
+ */
+// Single line
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  clicked() {
+    this.count++;
+  },
+};
+</script>
+
+<style>
+/*
+ Styling Section
+ */
+button {
+  background-color: darkorange;
+}
+</style>
