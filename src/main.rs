4dcbb5c6d4f4329defc8d248ea235de43bd7045f
Theo Danneels <danneels.theo@gmail.com>
2020-06-02 13:22:45 +0200

    style: inline sort variable

--- src/main.rs
+++ src/main.rs
@@ -72,11 +72,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     print_header(&mut stdout, &row, columns)?;
 
-    let sort = cli
-        .sort
-        .or(config.sort);
-
-    if let Some(sort_category) = sort {
+    if let Some(sort_category) = cli.sort.or(config.sort) {
         for (_, ref mut language) in &mut languages {
             language.sort_by(sort_category)
         }
