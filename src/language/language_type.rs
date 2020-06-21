2d063cf9ac3df7e646ba1e8b224f0d641049cf2a
Erin Power <xampprocky@gmail.com>
2020-06-21 21:15:08 +0200

    use par iter in jupyter

--- src/language/language_type.rs
+++ src/language/language_type.rs
@@ -244,8 +244,7 @@ impl LanguageType {
             })
             .unwrap_or(LanguageType::Python);
 
-        for cell in jupyter.cells {
-            let (language, stats) = match cell.cell_type {
+        let iter = jupyter.cells.par_iter().map(|cell| match cell.cell_type {
                 CellType::Markdown => (
                     LanguageType::Markdown,
                     LanguageType::Markdown.parse_from_str(cell.source.join(""), config),
@@ -254,8 +253,9 @@ impl LanguageType {
                     language,
                     language.parse_from_str(cell.source.join(""), config),
                 ),
-            };
+            }).collect::<Vec<_>>();
 
+        for (language, stats) in iter {
             *jupyter_stats.blobs.entry(language).or_default() += stats;
         }
 
