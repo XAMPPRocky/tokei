e608b7d3443683754e816c8312a9f8c516a9f9f3
VeryTastyTomato <8625408+VeryTastyTomato@users.noreply.github.com>
2020-11-30 07:56:58 +0100

    fix: shellcheck warnings (#663)
    SC2086: Double quote to prevent globbing and word splitting.
    SC2162: read without -r will mangle backslashes

--- ci/build.bash
+++ ci/build.bash
@@ -11,13 +11,12 @@ TARGET_TRIPLE=$2
 # $3 {boolean} = Whether or not building for release or not.
 RELEASE_BUILD=$3
 
-required_arg $CROSS 'CROSS'
-required_arg $TARGET_TRIPLE '<Target Triple>'
+required_arg "$CROSS" 'CROSS'
+required_arg "$TARGET_TRIPLE" '<Target Triple>'
 
 if [ -z "$RELEASE_BUILD" ]; then
-    $CROSS build --target $TARGET_TRIPLE
-    $CROSS build --target $TARGET_TRIPLE --all-features
+    $CROSS build --target "$TARGET_TRIPLE"
+    $CROSS build --target "$TARGET_TRIPLE" --all-features
 else
-    $CROSS build --target $TARGET_TRIPLE --all-features --release
+    $CROSS build --target "$TARGET_TRIPLE" --all-features --release
 fi
-
