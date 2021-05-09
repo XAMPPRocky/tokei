66967a1b9e42fb2d3887f4e091ab982aa0b3cb46
Michael Macnair <michael@macnair.org>
2021-05-09 19:05:14 +0100

    Fuzzing (#726)
    * Add fuzzing support
    
    See fuzz/README.md for details. Example bug: #725
    
    * improve fuzz docs
    
    * fuzzing: make relevant config part of input
    
    Most of the config is to do with what files to parse, however
    one setting - treat_doc_strings_as_comments - does impact parse_from_slice
    
    * fuzzing: improve docs + config clarity
    
    * fuzz/README.md: install instructions + another todo item

---
+++ fuzz/fuzz_targets/parse_from_slice.rs
@@ -0,0 +1,51 @@
+use arbitrary::Arbitrary;
+use std::str;
+
+use tokei::{Config, LanguageType};
+
+#[derive(Arbitrary, Debug)]
+pub struct FuzzInput<'a> {
+    lang: LanguageType,
+    treat_doc_strings_as_comments: bool,
+    data: &'a [u8],
+}
+
+// The first byte of data is used to select a language; remaining input is parsed
+// If check_total is true, asserts that the parsed stats pass a basic sanity test
+pub fn parse_from_slice(input: FuzzInput, check_total: bool) {
+    let config = &Config {
+        treat_doc_strings_as_comments: Some(input.treat_doc_strings_as_comments),
+
+        // these options don't impact the behaviour of parse_from_slice:
+        columns: None,
+        hidden: None,
+        no_ignore: None,
+        no_ignore_parent: None,
+        no_ignore_dot: None,
+        no_ignore_vcs: None,
+        sort: None,
+        types: None,
+    };
+
+    // check that parsing doesn't panic
+    let stats = input.lang.parse_from_slice(input.data, config);
+
+    if check_total {
+        // verify that the parsed total lines is not more than the total occurences of \n and \r\n.
+        // if/when all of the current discrepancies are fixed, we could make this stronger by checking it is equal.
+        if let Ok(s) = str::from_utf8(input.data) {
+            assert!(
+            stats.lines() <= s.lines().count(),
+            "{} got more total lines ({}) than str::lines ({}). Code: {}, Comments: {}, Blanks: {}. treat_doc_strings_as_comments: {}. File contents (as UTF-8):\n{}",
+            input.lang.name(),
+            stats.lines(),
+            s.lines().count(),
+            stats.code,
+            stats.comments,
+            input.treat_doc_strings_as_comments,
+            stats.blanks,
+            s
+        )
+        };
+    }
+}
--- src/language/language_type.tera.rs
+++ src/language/language_type.tera.rs
@@ -1,9 +1,11 @@
+use arbitrary::Arbitrary;
+
 /// Represents a individual programming language. Can be used to provide
 /// information about the language, such as multi line comments, single line
 /// comments, string literal syntax, whether a given language allows nesting
 /// comments.
 #[derive(Deserialize, Serialize)]
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Arbitrary, Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[non_exhaustive]
 pub enum LanguageType {
     {% for key, _ in languages -%}
